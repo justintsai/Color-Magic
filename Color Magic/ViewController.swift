23472125a35ee847582d5c2339a0dca45330634e
justintsai <justin710836@gmail.com>
2022-04-04 22:32:44 +0800

    Initial Commit

---
+++ Color Magic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Color Magic
+//
+//  Created by 蔡念澄 on 2022/4/4.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
